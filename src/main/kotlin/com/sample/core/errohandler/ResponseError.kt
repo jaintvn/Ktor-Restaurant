e8f531ab6c13efc44003be2ab9278cc95896789e
Jayanth <33921519+jaintvn@users.noreply.github.com>
2022-03-11 19:08:02 +0530

    #5 fix Changed error response to object array

--- src/main/kotlin/com/sample/core/errohandler/ResponseError.kt
+++ src/main/kotlin/com/sample/core/errohandler/ResponseError.kt
@@ -1,5 +1,7 @@
 package com.sample.core.errohandler
 
 data class ResponseErrors(val errors: Errors) {
-    data class Errors(val errors: List<String?> = listOf())
+    data class Errors(val errors: List<ErrorData?> = listOf())
 }
+
+data class ErrorData(val message: String?)
